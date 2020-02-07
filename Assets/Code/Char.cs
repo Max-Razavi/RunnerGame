c759ff87a3f517042a9072fb64f11341c7005b65
mrazavim <m.razavi.m@gmail.com>
2020-02-08 00:43:48 +0330

    first commit
    my project

---
+++ Assets/Code/Char.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char : MonoBehaviour
+{
+	
+	bool jump = false;
+	bool down = false;
+	bool grounded = false;
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+		if (Input.GetKeyDown (KeyCode.Space)){
+			jump = true;
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			down = true;		
+		}
+		
+		if (this.transform.position.y <= -30){
+			
+			Application.LoadLevel (0);
+		}
+    }
+	
+	void FixedUpdate () {
+		
+		//set the velosity of character
+		this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (5f, this.gameObject.GetComponent<Rigidbody2D>().velocity.y);
+		
+		if (jump && grounded) {
+			this.GetComponent<Animator>().speed = 0;
+			this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(300,750f));
+			jump = false;
+			grounded= false;
+		}
+		if (down) {
+			this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,-750));
+		}
+	}
+	void OnCollisionEnter2D(){
+		grounded =true;
+		this.GetComponent<Animator>().speed = 1;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		Destroy (other.gameObject);
+		this.GetComponent<AudioSource>().Play();
+		
+		this.score++;
+		this.UpdateScore();
+		
+	}
+	int score = 0;
+	public void UpdateScore(){
+		GameObject.Find("Score").GetComponent<UnityEngine.UI.Text>().text = "Score: "+ this.score; 
+	}
+	
+}
---
+++ Assets/Code/cameraControler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 characterPosition = GameObject.Find("Character").transform.position;
+        this.transform.position = new Vector3(characterPosition.x + 10, this.transform.position.y, characterPosition.z - 10);
+        
+    }
+}
